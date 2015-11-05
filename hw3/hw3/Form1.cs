e51858a847c0ff73862b9f049a0ab8924764291e
JessicaMaxey <zilainalynn14@gmail.com>
2015-11-04 22:26:04 -0800

    Fixed and finished key, working on encryption

--- hw3/hw3/Form1.cs
+++ hw3/hw3/Form1.cs
@@ -25,6 +25,19 @@ namespace hw3
         int[] IP_box = { 2, 6, 3, 1, 4, 8, 5, 7 };
         int[] IP_inverse_box = { 4, 1, 3, 5, 7, 2, 8, 6 };
         int[] EP_box = { 4, 1, 2, 3, 2, 3, 4, 1 };
+        int[] P8_starting_bits = { 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] four_starting_bits = { 1, 2, 3, 4 };
+        int[] EP_first_bits = { 4, 1, 2, 3 };
+        int[] EP_second_bits = { 2, 3, 4, 1 };
+        int[,] S0_box = { { 1, 0, 3, 2 },
+                          { 3, 2, 1, 0 },
+                          { 0, 2, 1, 3 },
+                          { 3, 1, 3, 2 }};
+
+        int[,] S1_box = { { 0, 1, 2, 3 },
+                          { 2, 0, 1, 3 },
+                          { 3, 0, 1, 0 },
+                          { 2, 1, 0, 3 }};
 
         public Form1()
         {
@@ -46,108 +59,35 @@ namespace hw3
             int[] five_starting_bits = { 1, 2, 3, 4, 5 };
             int[] shift_one_bits = { 2, 3, 4, 5, 1 };
             int[] shift_two_bits = { 3, 4, 5, 1, 2 };
-            int count = 0;
 
             //Do a P10
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    for (int j = 0; j < 10; j++)
-            //    {
-            //        if (starting_10bits[i] == P10[j])
-            //        {
-            //            new_key[j] = key[i];
-            //        }
-            //    }
-            //}
-
             new_key = PermutationBox(10, starting_10bits, P10, key);
 
-            //take output from that  and spilt into 5 bits
-            for (int i = 0; i < 5; i++)
-            {
-                left_shift[count++] = new_key[i];
-            }
-
-            count = 0;
-
-            for (int i = 5; i < 5; i++)
-            {
-                right_shift[count++] = new_key[i];
-            }
-
-
-            //
+            //split in half into 2 sets of 5 bits
+            Split(5, new_key, left_shift, right_shift);
 
             //then and do a 1 bit circular left shift on both
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    for (int j = 0; j < 5; j++)
-            //    {
-            //        if (five_starting_bits[i] == shift_one_bits[j])
-            //        {
-            //            first_left_shift[j] = left_shift[i];
-            //        }
-            //    }
-            //}
-
             first_left_shift = PermutationBox(5, five_starting_bits, shift_one_bits, left_shift);
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    for (int j = 0; j < 5; j++)
-            //    {
-            //        if (five_starting_bits[i] == shift_one_bits[j])
-            //        {
-            //            first_right_shift[j] = right_shift[i];
-            //        }
-            //    }
-            //}
-
             first_right_shift = PermutationBox(5, five_starting_bits, shift_one_bits, right_shift);
 
 
-            //take out put of that, recombine into 8 bits, take off the first 2 bits
-            //break down the IP'd plaintext into 2 sets of 4 bits
-            count = 2;
-            for (int i = 0; i < 3; i++)
-            {
-                first_P8[i] = first_left_shift[count++];
-            }
-
-            count = 0;
-
-            for (int i = 3; i < 8; i++)
-            {
-                first_P8[i] = first_right_shift[count++];
-            }
-
+            //recombine into 10 bits, take off the first 2 bits
+            first_P8 = RecombineBox8bits(3, 8, first_P8, first_left_shift, first_right_shift);
 
 
             //Do a P8 {6 3 7 4 8 5 10 9}
-            key_one = PermutationBox(8, starting_8bits, P8, first_P8);
+            key_one = PermutationBox(8, P8_starting_bits, P8, first_P8);
 
-
-            //output that key
 
             //with the output of the first LS-1, do a LS-2
             second_left_shift = PermutationBox(5, five_starting_bits, shift_two_bits, first_left_shift);
             second_right_shift = PermutationBox(5, five_starting_bits, shift_two_bits, first_right_shift);
 
-            //recombine and do another P8 {6 3 7 4 8 5 10 9}
-            count = 2;
-            for (int i = 0; i < 3; i++)
-            {
-                second_P8[i] = second_left_shift[count++];
-            }
+            //recombine and split off the leading 2 bits
+            second_P8 = RecombineBox8bits(3, 8, second_P8, second_left_shift, second_right_shift);
 
-            count = 0;
-
-            for (int i = 3; i < 8; i++)
-            {
-                second_P8[i] = second_right_shift[count++];
-            }
-
-            key_two = PermutationBox(8, starting_8bits, P8, second_P8);
+            //do another P8 {6 3 7 4 8 5 10 9}
+            key_two = PermutationBox(8, P8_starting_bits, P8, second_P8);
 
         }
 
@@ -169,59 +109,101 @@ namespace hw3
             return permutated_data;
         }
 
+        public int[] RecombineBox8bits (int first_loop_start, int second_loop_start, int[] combined_array, int[] left_array, int[] right_array)
+        {
+            int num_bits_off_front = 2;
+
+            for (int i = 0; i < first_loop_start; i++)
+            {
+                combined_array[i] = left_array[num_bits_off_front++];
+            }
+
+            num_bits_off_front = 0;
+
+            for (int i = 3; i < second_loop_start; i++)
+            {
+                combined_array[i] = right_array[num_bits_off_front++];
+            }
+
+            return combined_array;
+        }
+
+        public void Split (int array_size, int[] data_array, int[] left_shift, int[] right_shift)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array_size; i++)
+            {
+                left_shift[count++] = data_array[i];
+            }
+
+            count = 0;
+
+            for (int i = array_size; i < data_array.Length; i++)
+            {
+                right_shift[count++] = data_array[i];
+            }
+        }
+
         public void StartEncryption (int[] plaintext)
         {
             int count = 0;
 
             //Do the first IP on the original 
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    if (starting_8bits[i] == IP_box[j])
-                    {
-                        original_eight[j] = plaintext[i];
-                    }
-                }
-            }
-
-
+            original_eight = PermutationBox(8, starting_8bits, IP_box, plaintext);
 
             //break down the IP'd plaintext into 2 sets of 4 bits
-            for (int i = 0; i < 4; i++)
-            {
-                left_four[count++] = original_eight[i];
-            }
-
-            count = 0;
-
-            for (int i = 4; i < original_eight.Length; i++)
-            {
-                 right_four[count++] = original_eight[i];
-            }
-
-
+            Split(4, original_eight, left_four, right_four);
 
 
             //send to recursive function
-            Encryption(left_four, right_four);
+            Encryption(left_four, right_four, key_one);
 
         }
 
-        public void Encryption(int[] left_four, int[] right_four)
+        public void Encryption(int[] left_four, int[] right_four, int[]key)
         {
-            //take in 8 bit plaintext
-            //split into 2 sets of 4 bits
-            //LS will be stored for later
-            //RS is sent to E/P for expansion 
-            //E/P {4 1 2 3 2 3 4 1}
-            //that is then xOR'd with the K1
-            //split into 2 sets of 4 bits
-            //send through sboxs
-            //then do P4 {2 4 3 1}
-            //and xOR with the LS
+            int[] top_bits_xOR = new int[4];
+            int[] bottom_bits_xOR = new int[4];
+            int[] key_top_bits = new int[4];
+            int[] key_bottom_bits = new int[4];
+            int S0_row_index = 0;
+            int S0_col_index = 0;
+            int S1_row_index = 0;
+            int S1_col_index = 0;
+
+            //send the right 4 bits to the expander
+            int[] EP_top_bits = PermutationBox(4, four_starting_bits, EP_first_bits, right_four);
+            int[] EP_bottom_bits = PermutationBox(4, four_starting_bits, EP_second_bits, right_four);
+
+            //split the key into 2 sets of 4 bits and
+            //xOR the top expanded bits with the first 4 of key one's bits
+            Split(4, key, key_top_bits, key_bottom_bits);
+
+            for (int i = 0; i < 4; i++)
+            {
+                top_bits_xOR[i] = EP_top_bits[i] ^ key_top_bits[i];
+
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                bottom_bits_xOR[i] = EP_bottom_bits[i] ^ key_bottom_bits[i];
+
+            }
+
+            //1 and 4 are the rows, 2 and 3 are the col to be sent to the Sbox's
+            string top_row =  Convert.ToString(top_bits_xOR[0]) + Convert.ToString(top_bits_xOR[3]);
+            string top_col = Convert.ToString(top_bits_xOR[1]) + Convert.ToString(top_bits_xOR[2]);
+            string bottom_row = Convert.ToString(bottom_bits_xOR[0])+ Convert.ToString(bottom_bits_xOR[3]);
+            string bottom_col = Convert.ToString(bottom_bits_xOR[1])+ Convert.ToString(bottom_bits_xOR[2]);
+
+            for(int i = 0; i < 4; i++)
+            {
 
 
+
+            }
 
         }
 
@@ -232,8 +214,8 @@ namespace hw3
 
         public void run ()
         {
-            int[] plaintext = { 0, 0, 0, 0, 1, 1, 1, 1 };
-            int[] key = { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 };
+            int[] plaintext = { 1, 1, 1, 1, 0, 0, 1, 1 };
+            int[] key = { 1, 0, 1, 0, 0, 0, 0, 0, 1, 0 };
 
             GenerateKey(key);
 
