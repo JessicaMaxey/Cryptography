f867fab9f9250c2c68a691408ab19894702fbbd4
JessicaMaxey <zilainalynn14@gmail.com>
2015-11-27 12:56:24 -0800

    changed stuff and things

---
+++ hw4/rsa/rsa.h
@@ -0,0 +1,26 @@
+// rsa.h
+
+#pragma once
+
+using namespace System;
+
+namespace encryption {
+
+	public ref class rsa
+	{
+		rsa() = delete;
+		rsa(rsa ^) = delete;
+
+
+		// TODO: Add your methods for this class here.
+	};
+
+	public ref class diffie_hellman
+	{
+		diffie_hellman() = delete;
+		diffie_hellman(diffie_hellman ^) = delete;
+
+
+		// TODO: Add your methods for this class here.
+	};
+}
